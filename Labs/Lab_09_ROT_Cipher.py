8fbadb3780ff1ef22af049a32636a6b0969ee656
Jalisa <jalisa_calhoun@yahoo.com>
2021-01-16 03:36:54 -0800

    added Lab 9

---
+++ Labs/Lab_09_ROT_Cipher.py
@@ -0,0 +1,46 @@
+print('Lab 09: ROT Cipher')
+
+# create dictionary for each character and its corresponding character
+cipher = {
+    'a': 'n',
+    'b': 'o',
+    'c': 'p',
+    'd': 'q',
+    'e': 'r',
+    'f': 's',
+    'g': 't',
+    'h': 'u',
+    'i': 'v',
+    'j': 'w',
+    'k': 'x',
+    'l': 'y',
+    'm': 'z',
+    'n': 'a',
+    'o': 'b',
+    'p': 'c',
+    'q': 'd',
+    'r': 'e',
+    's': 'f',
+    't': 'g',
+    'u': 'h',
+    'v': 'i',
+    'w': 'j',
+    'x': 'k',
+    'y': 'l',
+    'z': 'm',
+}
+
+### VERSION 1 ###
+
+# request prompt from user
+statement = input('Give me a Phrase: ')
+
+
+# loop through the character of string entered by user
+character = ''
+for char in statement:
+    if char in cipher:
+       character += cipher[char]     
+
+# print result
+print(f'{statement} to {character}')
